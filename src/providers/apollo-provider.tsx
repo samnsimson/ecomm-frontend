07895d7385f240ee481673934ad8a07fd8776df4
samnsimson <samnishanth01@gmail.com>
2024-05-18 01:10:08 -0400

    apollo headers

--- src/providers/apollo-provider.tsx
+++ src/providers/apollo-provider.tsx
@@ -4,6 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
     uri: 'http://localhost:3000/graphql',
     cache: new InMemoryCache(),
+    headers: { authorization: '' },
 });
 
 export const ApolloClientProvider = ({ children }: { children: React.ReactNode }) => {
