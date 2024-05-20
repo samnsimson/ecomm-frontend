eeab14f770654b31f9e59f1396c9c14b829e57ea
samnsimson <samnishanth01@gmail.com>
2024-05-20 11:30:15 -0400

    removed apollo

--- src/components/list-users/index.tsx
+++ src/components/list-users/index.tsx
@@ -1,24 +1,10 @@
 'use client';
-import { useGetProductsQuery } from '@/graphql/generated';
-import { FC, HTMLAttributes } from 'react';
+import { FC, HTMLAttributes, useEffect } from 'react';
 
 interface ListUsersProps extends HTMLAttributes<HTMLDivElement> {
     [x: string]: any;
 }
 
 export const ListUsers: FC<ListUsersProps> = ({ ...props }) => {
-    const { data, loading } = useGetProductsQuery();
-
-    if (loading) return <p>Loaging...</p>;
-
-    return (
-        <div {...props}>
-            {data?.products.map((product) => (
-                <div key={product.id}>
-                    <h1>{product.title}</h1>
-                    <p>{product.description}</p>
-                </div>
-            ))}
-        </div>
-    );
+    return <div {...props}>Users</div>;
 };
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,13 +1,8 @@
 import { FC, PropsWithChildren } from 'react';
 import { UIProvider } from './ui-provider';
-import { ApolloProvider } from './apollo-provider';
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
-    return (
-        <UIProvider>
-            <ApolloProvider>{children}</ApolloProvider>
-        </UIProvider>
-    );
+    return <UIProvider>{children}</UIProvider>;
 };
 
 export default Providers;
