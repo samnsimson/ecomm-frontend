6ed40e6f2a1766c873ea59635887516be52ea7f1
samnsimson <samnishanth01@gmail.com>
2024-05-20 20:04:41 -0400

    raw

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,6 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { signInSchema } from './libs/zod/schemas';
-import { getClient } from './libs/apollo/client';
-import { LoginDocument, LoginMutationResult, LoginMutationVariables } from './graphql/generated';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
@@ -13,11 +11,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             },
             authorize: async (credentials) => {
                 try {
-                    const client = getClient();
                     const { username, password } = await signInSchema.parseAsync(credentials);
-                    const variables: LoginMutationVariables = { input: { username, password } };
-                    const { data } = await client.mutate<LoginMutationResult['data']>({ mutation: LoginDocument, variables });
-                    return data ? data.login : null;
+                    return null;
                 } catch (error: any) {
                     error.graphQLErrors.map((err: any) => console.log(err));
                     return null;
