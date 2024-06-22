dc506a062b391ccfff025bd0973b044a2932333a
samnsimson <samnishanth01@gmail.com>
2024-06-21 21:36:24 -0400

    refresh category page

--- src/app/(app)/(dashboard)/dashboard/categories/page.tsx
+++ src/app/(app)/(dashboard)/dashboard/categories/page.tsx
@@ -1,53 +1,36 @@
-import { ListCategoriesWithPagination } from '@/components/dashboard/categories/list';
-import { CreateCategoryForm } from '@/components/form/dashboard/categories/create';
+import { ListCategories } from '@/components/dashboard/categories/list';
 import { Page } from '@/components/page';
 import { buttonVariants } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { GetCategoriesDocument, GetCategoriesQuery, GetCategoriesQueryVariables } from '@/graphql/generated';
+import { gql } from '@/lib/graphql-client';
 import { cn } from '@/lib/utils';
+import { PlusIcon } from 'lucide-react';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import { ReactNode } from 'react';
+import { FC } from 'react';
 
-const renderComponent = (param: Map<string, string>): ReactNode | null => {
-    const action = param.get('action');
-    switch (action) {
-        case 'list':
-            return <ListCategoriesWithPagination />;
-        case 'edit':
-            return <></>;
-        case 'create':
-            return <CreateCategoryForm />;
-        default:
-            return <ListCategoriesWithPagination />;
-    }
-};
+const ActionButton: FC = () => (
+    <Link href={`dashboard/categories/create`} className={cn('flex items-center space-x-2', buttonVariants({ size: 'lg' }))}>
+        <PlusIcon />
+        <span>Create new category</span>
+    </Link>
+);
 
-const renderActionButton = (param: Map<string, string>) => {
-    const action = param.get('action');
-
-    const link = ({ path, title }: { path: string; title: string }) => (
-        <Link className={cn('flex space-x-3', buttonVariants({ variant: 'default', size: 'lg' }))} href={path}>
-            <span>{title}</span>
-        </Link>
-    );
-
-    switch (action) {
-        case 'list':
-            return link({ path: '/dashboard/categories?action=create', title: 'Create new category' });
-        case 'edit':
-            return <></>;
-        case 'create':
-            return link({ path: '/dashboard/categories?action=list', title: 'View all categories' });
-        default:
-            return link({ path: '/dashboard/categories?action=create', title: 'Create new category' });
-    }
-};
-
-const CategoriesPage: NextPage = ({ searchParams }: any) => {
-    const sp = new Map<string, string>(Object.entries(searchParams));
-
+const CategoriesPage: NextPage = async ({ searchParams }: Record<string, any>) => {
+    const { data } = await gql.request<GetCategoriesQuery, GetCategoriesQueryVariables>(GetCategoriesDocument);
+    if (!data) throw new Error('Error fetching categories');
     return (
-        <Page title="Categories" description="Manage all you categories" action={renderActionButton(sp)} className="flex flex-col space-y-6">
-            {renderComponent(sp)}
+        <Page title="Categories" description="Manage all you categories" action={<ActionButton />}>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Categories</CardTitle>
+                    <CardDescription>View and manage all product categories</CardDescription>
+                </CardHeader>
+                <CardContent className="p-0">
+                    <ListCategories categories={data.categories} />
+                </CardContent>
+            </Card>
         </Page>
     );
 };
--- src/components/dashboard/categories/list/index.tsx
+++ src/components/dashboard/categories/list/index.tsx
@@ -1,45 +1,42 @@
 'use client';
+import { DataTable } from '@/components/data-table';
 import { withPagination } from '@/components/hoc/pagination';
-import { List, ListItem } from '@/components/list';
-import { buttonVariants } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { badgeVariants } from '@/components/ui/badge';
+import { GetCategoriesQuery } from '@/graphql/generated';
 import { cn } from '@/lib/utils';
-import { useCategories } from '@/providers/caetgory.provider';
-import { PencilIcon } from 'lucide-react';
+import { ColumnDef } from '@tanstack/react-table';
 import Link from 'next/link';
 import { FC, HTMLAttributes } from 'react';
 
 interface ListCategoriesProps extends HTMLAttributes<HTMLDivElement> {
-    [x: string]: any;
+    categories: GetCategoriesQuery['categories'];
 }
 
-export const ListCategories: FC<ListCategoriesProps> = ({ ...props }) => {
-    const { categories } = useCategories();
-    return (
-        <Card className="divide-y" {...props}>
-            <CardHeader>
-                <CardTitle>All Categories</CardTitle>
-            </CardHeader>
-            <CardContent className="p-0">
-                <List>
-                    {categories.map((cat) => (
-                        <ListItem key={cat.id} className="prose flex min-w-full items-center justify-between px-6 py-4">
-                            <div className="space-y-0.5 ">
-                                <h4 className="my-0">{cat.title}</h4>
-                                <p className="my-0">{cat.description}</p>
-                            </div>
-                            <Link
-                                href={{ pathname: '/dashboard/categories', query: { action: 'edit' } }}
-                                className={cn('space-x-2 no-underline', buttonVariants({ variant: 'ghost' }))}
-                            >
-                                <PencilIcon size={14} /> <span>Edit</span>
-                            </Link>
-                        </ListItem>
-                    ))}
-                </List>
-            </CardContent>
-        </Card>
-    );
+type CategoryList = {
+    id: string;
+    title: string;
+    description: string;
+};
+
+const categoryData = (categories: GetCategoriesQuery['categories']) => {
+    return categories.map(({ id, title, description }) => ({ id, title, description: description || '-' }));
+};
+
+const columnDefs: Array<ColumnDef<CategoryList>> = [
+    { accessorKey: 'title', header: 'Title' },
+    { accessorKey: 'description', header: 'Description' },
+    {
+        header: 'Action',
+        cell: ({ row: { original } }) => (
+            <Link href={`dashboard/categories/edit/${original.id}`} className={cn(badgeVariants({ variant: 'warning' }))}>
+                Edit
+            </Link>
+        ),
+    },
+];
+
+export const ListCategories: FC<ListCategoriesProps> = ({ categories, ...props }) => {
+    return <DataTable columns={columnDefs} data={categoryData(categories)} />;
 };
 
 export const ListCategoriesWithPagination = withPagination(ListCategories);
